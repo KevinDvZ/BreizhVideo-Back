d5192a94cd27d044f59f417151799f685fd181bb
Kevin DvZ <kevin.deveza@gmail.com>
2020-12-21 09:48:54 +0100

    first commit

---
+++ src/test/java/fr/kevindvz/breizhvideo/BreizhvideoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kevindvz.breizhvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreizhvideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
